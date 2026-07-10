2c4e8e4bbaec721431e4d6ffbddc6dfc63909e36
Vikaswiom <design.3@wiom.in>
2026-07-10 18:54:33 +0530

    Use absolute python path in refresh wrapper (Task Scheduler PATH-safe)

--- run_refresh.cmd
+++ run_refresh.cmd
@@ -1,4 +1,4 @@
 @echo off
 cd /d "%~dp0"
-python "%~dp0refresh.py" >> "%~dp0refresh.log" 2>&1
+"C:\Users\Vikas Diwaker\AppData\Local\Programs\Python\Python310\python.exe" "%~dp0refresh.py" >> "%~dp0refresh.log" 2>&1
 echo [exit %ERRORLEVEL%] %DATE% %TIME% >> "%~dp0refresh.log"
